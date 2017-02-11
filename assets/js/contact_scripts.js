bfca2e3946ab31e8eecc61dc6e84aec8b2502b39
gundann <gundan.hr@gmail.com>
2017-02-11 15:41:17 +0530

    Original script

--- assets/js/contact_scripts.js
+++ assets/js/contact_scripts.js
@@ -75,6 +75,67 @@ $(document).ready(function(){
                 $("#name").addClass("error");
             }
 
-        } 
+        } else {
+
+            var data_of_form = $(this).serialize();
+
+            $.ajax({
+                url: 'assets/php/contact.php',
+                data: data_of_form,
+                type: 'POST',
+                success: function(data) {
+                    if ( data == "success" )
+                    {
+                        $("#user-name").val("");
+                        $("#user-email").val("");
+                        $("#user-message").val("");
+
+                        $(".info-message-form p").text("Message sent!");
+                        $(".info-message-form").addClass('success');
+                        setTimeout(
+                            function(){
+                                $(".info-message-form").removeClass('success');
+                                $(".info-message-form p").text("");
+                            }, 5000
+                        );
+                    } else {
+                        $(".info-message-form p").text("Error");
+                        $(".info-message-form").addClass('error');
+                        setTimeout(
+                            function(){
+                                $(".info-message-form").removeClass('error').fadeOut(500);
+                                $(".info-message-form p").text("");
+                            }, 5000
+                        );
+                    }
+                },
+                error: function(){
+                    alert("te1");
+                    $(".info-message-form p").text("Error");
+                    $(".info-message-form").addClass('error');
+                    setTimeout(
+                        function(){
+                            $(".info-message-form").removeClass('error');
+                            $(".info-message-form p").text("");
+                        }, 5000
+                    );
+                }
+            });
+
+        }
+
+        return false;
+    });
+
+    $(".close-msg").on("click", function() {
+        $(this).parent().removeClass("error");
+    });
+
+    $("#user-name, #user-email").on("click", function() {
+
+        $(this).removeClass("error");
+
+    });
+
 
 }); // End $(document).ready(function(){
